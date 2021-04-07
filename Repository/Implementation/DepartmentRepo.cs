3bea15a15d27f07b81ab273919a0989780d96f80
surakat gideon <surakatg@gmail.com>
2021-04-07 01:25:01 +0100

    depart

---
+++ Repository/Implementation/DepartmentRepo.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestApi_5._0.Model;
+using RestApi_5._0.Repository.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RestApi_5._0.Repository.Implementation
+{
+    public class DepartmentRepo : IDepartmentRepository
+    {
+        private readonly AppDbContext _context;
+
+        public DepartmentRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Department>> GetDepartments()
+        {
+            return await _context.Department.ToListAsync();
+        }
+
+        public async Task<Department> GetDepartment(int departmentId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Repository/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,12 @@
+﻿using RestApi_5._0.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RestApi_5._0.Repository.Interfaces
+{
+    public interface IDepartmentRepository
+    {
+        Task<IEnumerable<Department>> GetDepartments();
+        Task<Department> GetDepartment(int departmentId);
+    }
+}
