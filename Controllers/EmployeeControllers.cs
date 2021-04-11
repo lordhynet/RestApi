2b9521c5d49b26424c3fb89baccd919e7bf38d55
surakat gideon <surakatg@gmail.com>
2021-04-11 01:50:46 +0100

    byId

---
+++ Controllers/EmployeeControllers.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestApi_5._0.Repository.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace RestApi_5._0.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeControllers : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepo;
+
+        public EmployeeControllers(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepo = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetEmployee()
+        {
+            try
+            {
+                return Ok(await _employeeRepo.GetEmployees());
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retreivng data")
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmployeeById(int id)
+        {
+            try
+            {
+                var result = await _employeeRepo.GetEmployeeById(id);
+                if (result != null)
+                {
+
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retreivng data")
+            }
+        }
+    }
+}
