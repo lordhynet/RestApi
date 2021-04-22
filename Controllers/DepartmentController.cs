b336fb5deca2e101c03d9eec79e6666bf4715ad9
surakat gideon <surakatg@gmail.com>
2021-04-22 02:53:50 +0100

    deot

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestApi_5._0.Repository.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace RestApi_5._0.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IDepartmentRepository _deptRepo;
+
+        public DepartmentController(IDepartmentRepository departmentRepository)
+        {
+            _deptRepo = departmentRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetDepartments()
+        {
+            try
+            {
+                return Ok(await _deptRepo.GetDepartments());
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retreivng data");
+            }
+        }
+
+    }
+}
